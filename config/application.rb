fbce78a0abe82e1810935625a16155ce2a5ec0e6
Taiman Siddiqui <taiman.siddiqui@sazim.io>
2025-01-13 17:32:43 +0600

    fix(st-10): fix linting errors

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module SuperteacherBe
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
