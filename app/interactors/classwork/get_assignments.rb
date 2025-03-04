eb74ff712eccb64865e7d808bee87f80853b5e29
Taiman Siddiqui <taiman.siddiqui@sazim.io>
2025-03-05 02:05:45 +0600

    feat(main): Return empty classwork list rather than error response when no classwork is found

--- app/interactors/classwork/get_assignments.rb
+++ app/interactors/classwork/get_assignments.rb
@@ -1,21 +1,13 @@
 module Classwork
-    class GetAssignments
-      include Interactor
-      include Constants::ClassworkConstants
+  class GetAssignments
+    include Interactor
+    include Constants::ClassworkConstants
 
-      def call
-        assignments = Assignment.where(classroom_id: context.classroom_id)
-
-        if assignments.present?
-          context.assignments = assignments.to_a
-        else
-          context.fail!(
-            message: ASSIGNMENTS_RETRIEVE_FAIL,
-            status: :not_found
-          )
-        end
-      rescue StandardError => e
-        context.fail!(message: e.message, status: :unprocessable_entity)
-      end
+    def call
+      assignments = Assignment.where(classroom_id: context.classroom_id)
+      context.assignments = assignments.to_a
+    rescue StandardError => e
+      context.fail!(message: e.message, status: :unprocessable_entity)
     end
+  end
 end
--- app/interactors/classwork/get_materials.rb
+++ app/interactors/classwork/get_materials.rb
@@ -5,15 +5,7 @@ module Classwork
 
       def call
         materials = Material.where(classroom_id: context.classroom_id)
-
-        if materials.present?
-          context.materials = materials.to_a
-        else
-          context.fail!(
-            message: MATERIALS_RETRIEVE_FAIL,
-            status: :not_found
-          )
-        end
+        context.materials = materials.to_a
       rescue StandardError => e
         context.fail!(message: e.message, status: :unprocessable_entity)
       end
